22e0b2e15164602a1197b21585984b5da52cedb0
Jonathan Leo <jonathanleo@lawn-143-215-123-100.lawn.gatech.edu>
2018-09-07 10:24:00 -0400

    Had to revert back to previous commit...

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Jonathan
+ *  @author Bob
  *  @version 1.1
  */
 public class Person4 {
@@ -30,11 +30,8 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-        //Person 4 put your implementation here
-        for (i = 0; i < input.length(); i++) {
-            input.charAt(i) = (char)(input.charAt(i) + 1);
-        }
-        return input;
+      //Person 4 put your implementation here
+      return null;
     }
     
     /**
